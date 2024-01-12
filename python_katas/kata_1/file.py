b5c6afa9aa674cd1dc1c08498f5500ee9ce1e47c
ofirbakria <ofirbakria@gmail.com>
2024-01-12 15:57:18 +0200

    before starting anew branch

--- python_katas/kata_1/file.py
+++ python_katas/kata_1/file.py
@@ -2,10 +2,24 @@ import os
 
 def createfile(file):
     # isExisting = os.path.exists(file)
-    f = open(os.getcwd()+"\\python_katas\\kata_1\\"+file+".txt", "a")
+    f = open(os.getcwd()+"\\python_katas\\kata_1\\"+file, "a")
     f.write("Now the file has more content!\n")
     f.close()
     return None
 
 if __name__ == '__main__':
-    createfile("demo")
+    file="demo.txt"
+    createfile(file)
+    # Open the file for reading
+    with open("python_katas\\kata_1\\"+file, "r") as my_file:
+        # Initialize an empty list to store the lines
+        lines_list = []
+        
+        # Read and append each line to the list
+        for line in my_file:
+            lines_list.append(line.strip())  # strip() is used to remove newline characters
+        
+        # my_file.seek(0)
+
+    # Print the list of lines
+    print(lines_list)
