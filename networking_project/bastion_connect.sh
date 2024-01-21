7de81f81a4f4a9374e5b21d8513bc6c8311c4e79
daniel <dmakhalfeh@gmail.com>
2024-01-21 22:48:06 +0200

    bastion_connect.sh done

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -5,29 +5,35 @@ if [ -z "$KEY_PATH" ]; then
     echo "Error: KEY_PATH environment variable is not set."
     exit 5
 fi
-
+ssh_command="ssh -i $KEY_PATH "
 # Check if both public and private instance IPs are provided
 if [ $# -lt 1 ] || [ $# -gt 3 ] ; then
     echo "Usage: $0 <public-instance-ip> [<private-instance-ip>]"
     exit 5
 fi
-#variables
+
 public_instance_ip=$1
 private_instance_ip=$2
+case $# in
 
-if [ $# -eq 3 ]; then
-  com=$3
-
-fi
-
-ssh_command="ssh -i $KEY_PATH "
-#-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
-
-# Connect to the public instance first and then to the private instance if provided
-if [ -n "$private_instance_ip" ]; then
-    $ssh_command ubuntu@$public_instance_ip "export KEY_PATH=Daniel-key.pem && ssh -i $KEY_PATH ubuntu@$private_instance_ip $com"
-else
+  1)
     $ssh_command ubuntu@$public_instance_ip
-fi
+    ;;
+
+  2)
+    $ssh_command ubuntu@$public_instance_ip "ssh  $KEY_PATH ubuntu@$private_instance_ip"
+    ;;
+
+  3)
+    com=$#3
+    $ssh_command ubuntu@$public_instance_ip "export KEY_PATH=Daniel-key.pem && ssh -i $KEY_PATH ubuntu@$private_instance_ip $com"
+    ;;
+
+  *)
+    STATEMENTS
+    ;;
+esac
+
+
 
 # TODO your solution here
