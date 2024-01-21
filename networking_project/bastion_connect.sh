9ae209a69333fad4856e327a7afc509dc56c2cff
ofirbakria <ofirbakria@gmail.com>
2024-01-21 15:50:35 +0200

    first commit

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+public_ins="$1"
+private_ins="$2"
+ssh -i $KEY_PATH ubuntu@$public_ins "sudo ssh -i ofer-bakria-key02.pem ubuntu@$private_ins"
 
 
+# echo "$public_ins"
+# echo "$private_ins"
+
+ssh -i $KEY_PATH ubuntu@$public_ins
+
+# echo "$KEY_PATH"
+# echo "$?"
 # TODO your solution here
+# scp -i /path/to/private-key your-local-file.txt user@remote-server:/path/on/remote-server/
