1a838ce6f27f6d00b57637083c31279fdbcde6ba
ofirbakria <ofirbakria@gmail.com>
2024-01-21 19:46:59 +0200

    after connecting to the private instance directly

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+export KEY_PATH=~/Desktop/DevOps/atech-devops-nov-2023/networking_project/ofer-bakria-key.pem
 public_ins="$1"
 private_ins="$2"
-ssh -i $KEY_PATH ubuntu@$public_ins "sudo ssh -i ofer-bakria-key02.pem ubuntu@$private_ins"
+
+# if [ -z "${KEY_PATH}" ]; then
+    # echo "VAR is unset or set to the empty string"
+# else
+    # echo "VAR is set to some string"
+# fi
+
+# ssh -t -i $KEY_PATH ubuntu@$public_ins ". connect.sh 10.0.1.94"
+ssh -t -i $KEY_PATH ubuntu@$public_ins "ssh -i ofer-bakria-key02.pem ubuntu@10.0.1.94"
+
 
 
 # echo "$public_ins"
 # echo "$private_ins"
-
-ssh -i $KEY_PATH ubuntu@$public_ins
 
 # echo "$KEY_PATH"
 # echo "$?"
