14296ea425973dbf2f30f098ddb18fec430e5b44
ofirbakria <ofirbakria@gmail.com>
2024-01-22 10:19:28 +0200

    adding if else for bastion_connect.sh file

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -3,6 +3,15 @@ export KEY_PATH=~/Desktop/DevOps/atech-devops-nov-2023/networking_project/ofer-b
 public_ins="$1"
 private_ins="$2"
 
+
+if [ "$#" -eq 1 ]; then
+    echo "1"
+elif [ "$#" -eq 2 ]; then
+    echo "2"
+else
+    echo "3"
+fi
+
 # if [ -z "${KEY_PATH}" ]; then
     # echo "VAR is unset or set to the empty string"
 # else
@@ -10,12 +19,8 @@ private_ins="$2"
 # fi
 
 # ssh -t -i $KEY_PATH ubuntu@$public_ins ". connect.sh 10.0.1.94"
-ssh -t -i $KEY_PATH ubuntu@$public_ins "ssh -i ofer-bakria-key02.pem ubuntu@10.0.1.94"
+ssh -t -i $KEY_PATH ubuntu@$public_ins "ssh -i ofer-bakria-key02.pem ubuntu@10.0.1.94 'ls -la'"
 
-
-
-# echo "$public_ins"
-# echo "$private_ins"
 
 # echo "$KEY_PATH"
 # echo "$?"
