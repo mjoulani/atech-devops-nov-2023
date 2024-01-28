b9bcc0cd0bcee597f484f604a6981e3558a22d6f
Nour <74349478+bsnoor@users.noreply.github.com>
2024-01-28 19:40:49 +0200

    Update bastion_connect.sh

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ -z "$KEY_PATH" ] || [ ! -f "$KEY_PATH" ]; then
+if [[ -z "$KEY_PATH" ]]; then
   echo "Error"
   exit 5
 fi
@@ -19,7 +19,7 @@ validate_ip "$BASTION_IP"
 if [ $# -eq 2 ]; then
   PRIVATE_IP=$2
   validate_ip "$PRIVATE_IP"
-  ssh -i "$KEY_PATH" -o "ProxyJump=$BASTION_IP" ubuntu@"$PRIVATE_IP"
+  ssh -o ProxyCommand="ssh -W %h:%p ubuntu@$BASTION_IP -i $KEY_PATH" -i $KEY_PATH ubuntu@"$PRIVATE_IP"
 else
   # Connect to the public instance
   ssh -i "$KEY_PATH" ubuntu@"$BASTION_IP"
