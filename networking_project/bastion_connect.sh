276ef61f4a0c41ec32d51520af17fc56683da59e
Hadeel Safadi <122454154+hadeelsaf@users.noreply.github.com>
2024-01-29 11:34:59 +0200

    Update bastion_connect.sh

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
-
-
-# TODO your solution here
+if [[ -z "$KEY_PATH" ]]; then
+  echo "Error key path!!"
+  exit 5
+fi
+if [ $# -lt 1 ]; then
+  echo "Error Please provide bastion IP address"
+  exit 5
+fi
+validate_ip() {
+  local ip="$1"
+  if [[ ! $ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "Error."
+    exit 5
+  fi
+}
+BASTION_IP=$1
+validate_ip "$BASTION_IP"
+if [ $# -eq 2 ]; then
+  PRIVATE_IP=$2
+  validate_ip "$PRIVATE_IP"
+  ssh -o ProxyCommand="ssh -W %h:%p ubuntu@$BASTION_IP -i $KEY_PATH" -i $KE>else
+  # Connect to the public instance
+  ssh -i "$KEY_PATH" ubuntu@"$BASTION_IP"
+fi
