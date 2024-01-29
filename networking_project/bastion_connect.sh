3de161dd7d92012f009767abf22141ee70bd70ba
Ehab Aburesh <110368166+ehababuresh@users.noreply.github.com>
2024-01-29 21:20:44 +0200

    het

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -1,24 +1,35 @@
+
+
 #!/bin/bash
 export KEY_PATH
-read -s KEY_PATH < PATH.txt
+read -s KEY_PAqTH < PATH.txt
+1
+bastion_ip=$1
+private_ip=$2
+remote_command=$3
 
-public_key=$1
-private_key=$2
-if [ -z "$1" ];then
-echo "Please provide bastion IP address"
-exit 5
+# בדיקה אם ניתנה כתובת IP של בסטיון
+if [ -z "$bastion_ip" ]; then
+    echo "Please provide bastion IP address"
+    exit 5
 fi
-if [ -z "$KEY_PATH" ];then
-echo "KEY_PATH env var is expected"
-exit 5
+
+# בדיקה שהמשתנה KEY_PATH מוגדר
+if [ -z "$KEY_PATH" ]; then
+    echo "KEY_PATH env var is `expected"
+    exit 5
 fi
+
+# הוספת מפתח SSH ל-agent
 ssh-add $KEY_PATH
-if [ ! -z "$3" ]; then
-    ssh -t -A ubuntu@$public_key "ssh ubuntu@$private_key '$3'"
-elif [ ! -z "$2" ]; then
-    ssh -t -A ubuntu@$public_key "ssh ubuntu@$private_key"
-elif [ -z "$2" ]; then
-    ssh -t -A ubuntu@$public_key
-fi
 
-#ssh -t -i $KEY_PATH ubuntu@$public_ins ". connect.sh "
+# בדיקה אם ניתנה פקודה לביצוע בשרת הפרטי
+if [ ! -z "$remote_command" ]; then
+    ssh -t -A ubuntu@$bastion_ip "ssh ubuntu@$private_ip '$remote_command'"
+# בדיקה אם ניתנה כתובת IP של שרת פרטי
+elif [ ! -z "$private_ip" ]; then
+    ssh -t -A ubuntu@$bastion_ip "ssh ubuntu@$private_ip"
+# אם לא ניתנה כתובת IP של שרת פרטי
+else
+    ssh -t -A ubuntu@$bastion_ip
+fi
