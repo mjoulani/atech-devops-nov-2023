6e5dc6ce4c88c37885c62603c17bca6d94a71ffb
Hadeel Safadi <122454154+hadeelsaf@users.noreply.github.com>
2024-01-29 12:04:01 +0200

    Update ssh_keys_rotation.sh

--- networking_project/ssh_keys_rotation.sh
+++ networking_project/ssh_keys_rotation.sh
@@ -1,1 +1,28 @@
+#!/bin/bash
+private_ip=$1
+echo "private_ip: $private_ip"
+
+newkey="your_key_filename"  # Replace with your desired key filename
+
+if [[ -z $KEY_PATH ]];then
+   echo "KEY_PATH variable not found."
+   exit 5
+fi
+
+if [[ -z $private_ip ]];then
+  echo "Please provide IP address!!"
+  exit 5
+fi
+
+if [[ ! -f ~/$newkey ]]; then
+    ssh-keygen -f ~/$newkey -t rsa -N '' <<< "y" 1> /dev/null
+fi
+
+chmod 600 ~/$newkey.pub
+scp -o StrictHostKeyChecking=no -i $KEY_PATH ~/$newkey.pub $user@$private_i>
+if [[ -f ~/$newkey ]]; then
+    mv ~/$newkey{,.bak}
+    ssh-keygen -f ~/$newkey -t rsa -N '' <<< "y" 1> /dev/null
+    scp -o StrictHostKeyChecking=no -i ~/$newkey.bak ~/$newkey.pub $user@$p>    rm ~/$newkey.bak
+fi
 
