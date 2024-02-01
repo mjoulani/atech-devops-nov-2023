d2952e418e3ec129fbb1c9c95bb54a27a4312ea9
Abedgz <gz.abed@gmail.com>
2024-02-01 09:48:38 +0200

    abed Final

--- docker/mongo/mongo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Pull the latest MongoDB image
-docker pull mongo:latest
-
-# Run the MongoDB container
-docker pull mongodb/mongodb-community-server
-docker run --name mongodb -p 27017:27017 -v E:\elements\Devops_Atech\mongo_data:/data/db -e MONGO_INITDB_ROOT_USERNAME=admin -e MONGO_INITDB_ROOT_PASSWORD=password -d mongo:latest
