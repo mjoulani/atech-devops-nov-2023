cdbeb47f14a7be61568d221dd76b02cf698e1e77
Firas Naarani <firas.narani.1999@gmail.com>
2024-02-02 15:41:01 +0200

    bastion_connect.sh > Done

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -1,4 +1,37 @@
 #!/bin/bash
+PubIP=$1
+PrvIP=$2
+cmd=$3
+User=ubuntu
 
+#CheckCase1
+if [[ -z $KEY_PATH ]];then
+    echo "KEY_PATH env var is expected"
+    exit 5
+fi
 
-# TODO your solution here
+#CheckCase2
+if [[ -z $PubIP ]];then
+    echo "Please provide bastion IP address"
+    exit 5
+fi
+
+#TODO: Automatic Copy KEY.pem to PubIP
+# scp  -o StrictHostKeyChecking=no -i ${KEY_PATH} $KEY_PATH ${User}@${PubIP}:~
+# ssh -o StrictHostKeyChecking=no -i "${KEY_PATH}" "${User}@${PubIP}" "chmod 600 ~/$(basename "${KEY_PATH}")"
+
+case $# in
+    1)
+        ssh -q -o StrictHostKeyChecking=no -i ${KEY_PATH} ${User}@${PubIP}
+        ;;
+    2)
+        ssh -q -o StrictHostKeyChecking=no -i ${KEY_PATH} ${User}@${PubIP} -t "ssh -q -o StrictHostKeyChecking=no -i \${KEY_PATH} ${User}@${PrvIP} -t"
+        ;;
+    3)
+        ssh -q -o StrictHostKeyChecking=no -i ${KEY_PATH} ${User}@${PubIP} -t "ssh -q -o StrictHostKeyChecking=no -i \${KEY_PATH} ${User}@${PrvIP} -t $cmd"
+        ;;
+    *)
+        echo "More than three parameters passed."
+        exit
+        ;;
+esac
