76bf058738fa6ca30418fb20c8f2c4c33ea77c71
Timor4K <timor.zaid@gmail.com>
2024-02-03 21:37:57 +0200

    Network Project Solution

--- networking_project/tlsHandshake.sh
+++ networking_project/tlsHandshake.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-# Step 1: Client Hello HTTP Request
-echo "Step 1: Sending Client Hello HTTP Request..."
-client_hello_response=$(curl -s -X POST "http://${server_hostname}:${server_port}/clienthello" \
-    -d '{"version": "1.3", "ciphersSuites": ["TLS_AES_128_GCM_SHA256", "TLS_CHACHA20_POLY1305_SHA256"], "message": "Client Hello"}')
+# step 1: client Hello HTTP request
+echo "Step 1: Sending Client Hello HTTP request..."
+CLIENT_HELLO_RESPONSE=$(curl -s -X POST "http://35.78.94.169:8080/clienthello" \
+    -h "Content-Type: application/json" \
+    -d '{"VERSION": "1.3", "CIPHERS_SUITES": ["TLS_AES_128_GCM_SHA256", "TLS_CHACHA20_POLY1305_SHA256"], "MESSAGE": "Client Hello"}')
 
-# Check if the request was successful
+# check if the request was successful
 if [ $? -ne 0 ]; then
-    echo "Error sending Client Hello HTTP Request."
+    echo "Error sending Client Hello HTTP request."
     exit 2
 fi
 
-# Parse Server Hello response using jq
-version=$(echo "$client_hello_response" | jq -r '.version')
-cipherSuite=$(echo "$client_hello_response" | jq -r '.cipherSuite')
-sessionID=$(echo "$client_hello_response" | jq -r '.sessionID')
-serverCert=$(echo "$client_hello_response" | jq -r '.serverCert')
+# parse server Hello response using jq
+VERSION=$(echo "$CLIENT_HELLO_RESPONSE" | jq -r '.VERSION')
+CIPHER_SUITE=$(echo "$CLIENT_HELLO_RESPONSE" | jq -r '.CIPHER_SUITE')
+SESSION_ID=$(echo "$CLIENT_HELLO_RESPONSE" | jq -r '.SESSION_ID')
+SERVER_CERT=$(echo "$CLIENT_HELLO_RESPONSE" | jq -r '.SERVER_CERT')
 
 echo "Received Server Hello response:"
 echo "Version: $version"
@@ -23,43 +24,50 @@ echo "Cipher Suite: $cipherSuite"
 echo "Session ID: $sessionID"
 echo "Server Certificate: $serverCert"
 
-# Step 2: Verify Server Certificate
-echo "Step 2: Verifying Server Certificate..."
-wget -O cert-ca-aws.pem "$ca_cert_url"
-openssl verify -CAfile cert-ca-aws.pem "$serverCert"
+# step 2: verify server certificate
+echo "Step 2: Verifying server certificate..."
+wget https://raw.githubusercontent.com/alonitac/atech-devops-june-2023/main/networking_project/tls_webserver/cert-ca-aws.pem -O cert-ca-aws.pem
+openssl verify -CAfile cert-ca-aws.pem <<< "$SERVER_CERT"
 
-# Check if certificate verification was successful
+# check if certificate verification was successful
 if [ $? -ne 0 ]; then
-    echo "Server Certificate is invalid."
+    echo "Server certificate is invalid."
     exit 5
 fi
 
-echo "Server Certificate is valid."
+echo "Server certificate is valid."
 
-# Step 3: Generate 32 random bytes base64 string as master-key
+# step 3: Generate 32 random bytes base64 string as master-key
 echo "Step 3: Generating 32 random bytes base64 string as master-key..."
 masterKey=$(openssl rand -base64 32)
 
 echo "Master Key: $masterKey"
 
-# Step 4: Encrypt master-key with server certificate
-echo "Step 4: Encrypting Master Key with Server Certificate..."
-encryptedMasterKey=$(echo -n "$masterKey" | openssl smime -encrypt -aes-256-cbc -outform DER -recip "$serverCert" | base64 -w 0)
+# step 4: Encrypt master-key with server certificate
+echo "Step 4: Encrypting master key with server certificate..."
+encrypted_masterKey=$(echo -n "$masterKey" | openssl smime -encrypt -aes-256-cbc -outform DER -recip "$SERVER_CERT" | base64 -w 0)
 
-# Step 5: Perform key exchange with the server
-echo "Step 5: Performing Key Exchange with Server..."
-keyExchangeResponse=$(curl -s -X POST "http://${server_hostname}:${server_port}/keyexchange" \
-    -d "{\"sessionID\": \"$sessionID\", \"masterKey\": \"$encryptedMasterKey\", \"sampleMessage\": \"Hi server, please encrypt me and send to client!\"}")
+# step 5: Perform key exchange with the server
+echo "Step 5: Performing key exchange with Server..."
+key_exchange_response=$(curl -s -X POST "http://${server_hostname}:${server_port}/keyexchange" \
+    -d "{\"SESSION_ID\": \"$SESSION_ID\", \"masterKey\": \"$encrypted_masterKey\", \"sample_message\": \"Hi server, please encrypt me and send to the client!\"}")
+#send the encripted master key
+RESPONSE=$(curl -s -X POST -H "Content-Type: application/json" -d "$KEY_EXCHANGE" http://35.78.94.169:8080/keyexchange)
+#check if the key exchange pass
+if [ -z "$RESPONSE" ]; then
+    echo "Error: Failed to perform key exchange."
+    exit 1
+fi
 
-# Parse Key Exchange response using jq
-encryptedSampleMessage=$(echo "$keyExchangeResponse" | jq -r '.encryptedSampleMessage')
+# parse key exchange response using jq
+encrypted_sample_message=$(echo "$key_exchange_response" | jq -r '.encrypted_sample_message')
 
-# Step 6: Decrypt the sample message using the master-key
-echo "Step 6: Decrypting Sample Message using Master Key..."
-echo "$encryptedSampleMessage" | base64 -d > encSampleMsgReady.txt
-decryptedSampleMessage=$(openssl enc -d -aes-256-cbc -in encSampleMsgReady.txt -base64 -K $(echo -n "$masterKey" | base64 -d) -iv 0)
+# step 6: Decrypt the sample message using the master-key
+echo "Step 6: Decrypting sample message using master key..."
+echo "$encrypted_sample_message" | base64 -d > enc_sample_msg_ready.txt
+decrypted_sample_message=$(openssl enc -d -aes-256-cbc -in enc_sample_msg_ready.txt -base64 -K $(echo -n "$masterKey" | base64 -d) -iv 0)
 
-# Check if decryption was successful
+# check if decryption was successful
 if [ $? -ne 0 ]; then
     echo "Step 6: Server symmetric encryption using the exchanged master-key has failed."
     exit 6
@@ -67,7 +75,7 @@ fi
 
 echo "Step 6: Server symmetric encryption using the exchanged master-key successful."
 
-# Additional steps (if needed) can be added here
+# final
 
 echo "Client-Server TLS handshake has been completed successfully."
 exit 0
