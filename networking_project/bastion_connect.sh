878adfd158113c5da61db903c62a9b2c7f55d8fe
muhammad Qadora <memomq70@gmail.com>
2024-02-04 19:59:33 +0000

    finitoo

--- networking_project/bastion_connect.sh
+++ networking_project/bastion_connect.sh
@@ -17,9 +17,19 @@ if [[ -z $PubIP ]];then
     exit 5
 fi
 
+
+ssh -q -o StrictHostKeyChecking=no -i ${KEY_PATH} ${User}@${PubIP} -t "bash -s" <<EOF
+#!/bin/bash
+grep 'export KEY' .bashrc 1> /dev/null
+if [[ "\$?" != "0" ]];then
+sed -i '1s/^/export KEY_PATH="$(basename $KEY_PATH)"\\n/' .bashrc
+fi
+EOF
+
 #Check if 'Key_Rotaion' Exists
-checkKeyRotation=$(ssh -q -o StrictHostKeyChecking=no -i ${KEY_PATH} ${User}@${PubIP} -t 'ls ssh_keys_rotation.sh 2> /dev/null | wc -l')
-if [[ "$checkKeyRotation" != "1" ]];then
+c=$(ssh -q -o StrictHostKeyChecking=no -i ${KEY_PATH} ${User}@${PubIP} -t 'ls ssh_keys_rotation.sh 2> /dev/null | wc -l')
+c=$(echo $c | tr -d '\r')
+if [[ $c != 1 ]];then
     scp -q -o StrictHostKeyChecking=no -i ${KEY_PATH} $KeyRot ubuntu@${PubIP}:~
     echo "COPIED KEY ROTATION SCRIPT TO $PubIP"
     ssh -q -o StrictHostKeyChecking=no -i ${KEY_PATH} ${User}@${PubIP} -t "chmod +x $KeyRot"
@@ -27,21 +37,16 @@ if [[ "$checkKeyRotation" != "1" ]];then
 fi
 
 #Check if 'KEY_PATH' Exists
-checkKeyPath=$(ssh -q -o StrictHostKeyChecking=no -i ${KEY_PATH} ${User}@${PubIP} -t 'ls "$KEY_PATH" 2> /dev/null | wc -l')
-if [[ "$checkKeyPath" != "1" ]];then
+cpath=$(ssh -q -o StrictHostKeyChecking=no -i ${KEY_PATH} ${User}@${PubIP} -t 'ls "$KEY_PATH" 2> /dev/null | wc -l')
+cpath=$(echo $cpath | tr -d '\r')
+if [[ $cpath != 1 ]];then
     scp -q -o StrictHostKeyChecking=no -i ${KEY_PATH} $KEY_PATH ubuntu@${PubIP}:~
     echo "COPIED KEY TO $PubIP"
-    ssh -q -o StrictHostKeyChecking=no -i ${KEY_PATH} ${User}@${PubIP} -t "chmod 600 $KEY_PATH"
+    ssh -q -o StrictHostKeyChecking=no -i ${KEY_PATH} ${User}@${PubIP} -t "chmod 600 \$KEY_PATH"
     echo -e "ADDED PERMISTIONS\n####################"
 fi
 
-ssh -q -o StrictHostKeyChecking=no -i ${KEY_PATH} ${User}@${PubIP} -t "bash -s" <<EOF
-#!/bin/bash
-grep "$KEY_PATH" .bashrc 1> /dev/null
-if [[ "\$?" != "0" ]];then
-sed -i '1s/^/export KEY_PATH="$KEY_PATH"\\n/' .bashrc
-fi
-EOF
+
 
 case $# in
     1)
