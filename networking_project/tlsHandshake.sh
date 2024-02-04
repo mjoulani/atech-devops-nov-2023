463cfc97867ac21455ddcf942c0780ded59a14c0
SamerJamamaa <150609638+SamerJamamaa@users.noreply.github.com>
2024-02-04 20:33:49 +0200

    Update tlsHandshake.sh

--- networking_project/tlsHandshake.sh
+++ networking_project/tlsHandshake.sh
@@ -1,66 +1,63 @@
 #!/bin/bash
 
-# Step 1 - Client Hello
-client_hello_response=$(curl -s -X POST -H "Content-Type: application/json" \
-  -d '{"version": "1.3", "ciphersSuites": ["TLS_AES_128_GCM_SHA256", "TLS_CHACHA20_POLY1305_SHA256"], "message": "Client Hello"}' \
-  http://65.1.133.40/clienthello)
+# # Step 1 - Client Hello
 
-echo $client_hello_response > client_response
+#Client Hello HTTP request to the server and save serverCert from the JSON response
+curl -X POST \
+      http://13.201.31.184:8080/clienthello \
+      -H "Content-Type: application/json" \
+      -d '{"version":"1.3","ciphersSuites":["TLS_AES_128_GCM_SHA256","TLS_CHACHA20_POLY1305_SHA256"],"message":"Client Hello"}' \
+      | jq -r '.serverCert' > cert.pem
 
-# Check if the response is valid JSON
-if ! jq '.' <<< "${client_hello_response}" &> /dev/null; then
-  echo "Invalid response from server. Exiting."
-  exit 1
-fi
-echo "Response is valid"
 
-# Parse Client Hello response
-#version=$(jq -r '.version' <<< "${client_hello_response}")
-#cipher_suite=$(jq -r '.cipherSuite' <<< "${client_hello_response}")
-session_id=$(jq -r '.sessionID' <<< "${client_hello_response}")
-server_cert=$(jq -r '.serverCert' <<< "${client_hello_response}")
+#Client Hello HTTP request to the server and save sessionID from the JSON response
+curl -X POST \
+      http://13.201.31.184:8080/clienthello \
+      -H "Content-Type: application/json" \
+      -d '{"version":"1.3","ciphersSuites":["TLS_AES_128_GCM_SHA256","TLS_CHACHA20_POLY1305_SHA256"],"message":"Client Hello"}' \
+      | jq -r '.sessionID' > sessionID.txt
 
-echo $server_cert > cert.pem
+
+SESSION_ID=$(cat sessionID.txt)
+
+
+# download Certificate Authority from Amazon Web Services
+wget  https://raw.githubusercontent.com/alonitac/atech-devops-june-2023/main/networking_project/tls_webserver/cert-ca-aws.pem
 
 # Step 2 - Server Certificate Verification
-if ! openssl verify -CAfile cert-ca-aws.pem <<< "${server_cert}" &> /dev/null; then
-#if ! openssl verify -CAfile cert-ca-aws.pem cert.pem &> /dev/null; then
+#VERFY=$(openssl verify -CAfile cert-ca-aws.pem cert.pem)
+#if [ "$VERFY" != "cert.pem: OK" ]; then
+if [ $(openssl verify -CAfile cert-ca-aws.pem cert.pem) != "cert.pem: OK" ]; then
   echo "Server Certificate is invalid. Exiting."
   exit 5
 fi
 
-echo "cert.pem: OK"
-
 # Step 3 - Generate 32 random bytes base64 string as the master-key
-generated_key=$(openssl rand -base64 32)
+openssl rand -base64 32 > masterKey.txt
 
 # Step 4 - Encrypt the master-key with the server certificate
-master_key=$(echo "${generated_key}" | openssl smime -encrypt -aes-256-cbc \
-  -outform DER <(echo "${server_cert}" | base64 -d) | base64 -w 0)
+master_key=$(openssl smime -encrypt -aes-256-cbc -in masterKey.txt -outform DER cert.pem | base64 -w 0)
 
-#master_key=$(echo "${generated_key}" | openssl smime -encrypt -aes-256-cbc \
- # -outform DER cert.pem | base64 -d) | base64 -w 0)  
-
-# Step 5 - Client-Server master-key exchange
-key_exchange_response=$(curl -s -X POST -H "Content-Type: application/json" \
-  -d '{"sessionID": "'"${session_id}"'", "masterKey": "'"${master_key}"'", "sampleMessage": "Hi server, please encrypt me and send to client!"}' \
-  http://<public-ec2-instance-ip>:8080/keyexchange)
+# # Step 5 - Client-Server master-key exchange
+curl -X POST \
+      http://13.201.31.184:8080/keyexchange \
+      -H "Content-Type: application/json" \
+      -d '{"sessionID":"'${SESSION_ID}'","masterKey":"'${master_key}'","sampleMessage":"Hi server, please encrypt me and send to client!"}'\
+      | jq -r '.encryptedSampleMessage' > encryptedSampleMessage.txt 
 
 # Parse Key Exchange response
-session_id_response=$(jq -r '.sessionID' <<< "${key_exchange_response}")
-encrypted_sample_message=$(jq -r '.encryptedSampleMessage' <<< "${key_exchange_response}")
-
-# Decode the encrypted sample message
-echo "${encrypted_sample_message}" | base64 -d > encSampleMsgReady.txt
+cat encryptedSampleMessage.txt | base64 -d > encSampleMsgReady.txt
 
 # Decrypt the sample message using the master-key
-decrypted_sample_message=$(openssl enc -d -aes-256-cbc -in encSampleMsgReady.txt -K $(echo "${generated_key}" | base64 -d) -iv 0)
+openssl enc -d -aes-256-cbc -pbkdf2  -kfile masterKey.txt -in encSampleMsgReady.txt -out dencryptedSampleMessage.txt
 
-# Step 6 - Client verification message
+decrypted_sample_message=$(cat dencryptedSampleMessage.txt)
+
+# # Step 6 - Client verification message
 if [ "${decrypted_sample_message}" != "Hi server, please encrypt me and send to client!" ]; then
-  echo "Server symmetric encryption using the exchanged master-key has failed. Exiting."
-  exit 6
-fi
+ echo "Server symmetric encryption using the exchanged master-key has failed. Exiting."
+ exit 6
+ fi
 
-# If everything is ok, print a positive message
-echo "Client-Server TLS handshake has been completed successfully"
+#If everything is ok, print a positive message
+ echo "Client-Server TLS handshake has been completed successfully"
