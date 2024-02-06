2cbaea383994f335457a06f609f9790786aaf502
alexey <mihaylov>
2024-02-06 19:20:09 +0200

    update demos

--- docker/custom_images/flask/app.py
+++ docker/custom_images/flask/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello():
     return "Hello, Docker with Flask!"
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
