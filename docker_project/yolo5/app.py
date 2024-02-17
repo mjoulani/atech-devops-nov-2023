85043eeda33eb39624661c0ac145f0aa11a8f884
aburesh <110368166+ehababuresh@users.noreply.github.com>
2024-02-17 23:52:16 +0200

    eeee

--- docker_project/yolo5/app.py
+++ docker_project/yolo5/app.py
@@ -1,11 +1,13 @@
 import time
 from pathlib import Path
-from flask import Flask, request
+import boto3
+from flask import Flask, request, jsonify
 from detect import run
 import uuid
 import yaml
 from loguru import logger
 import os
+import pymongo
 
 images_bucket = os.environ['BUCKET_NAME']
 
@@ -13,6 +15,7 @@ with open("data/coco128.yaml", "r") as stream:
     names = yaml.safe_load(stream)['names']
 
 app = Flask(__name__)
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -26,7 +29,13 @@ def predict():
 
     # TODO download img_name from S3, store the local image path in original_img_path
     #  The bucket name should be provided as an env var BUCKET_NAME.
-    original_img_path = ...
+
+    bucket_name = os.getenv('BUCKET_NAME')
+
+    original_img_path = str(img_name)
+
+    s3 = boto3.client('s3')
+    s3.download_file(bucket_name, img_name, original_img_path)
 
     logger.info(f'prediction: {prediction_id}/{original_img_path}. Download img completed')
 
@@ -48,6 +57,9 @@ def predict():
 
     # TODO Uploads the predicted image (predicted_img_path) to S3 (be careful not to override the original image).
 
+    the_image = original_img_path[:-4] + "_predicted.jpg"
+    s3.upload_file(str(predicted_img_path), bucket_name, the_image)
+
     # Parse prediction labels and create a summary
     pred_summary_path = Path(f'static/data/{prediction_id}/labels/{original_img_path.split(".")[0]}.txt')
     if pred_summary_path.exists():
@@ -65,18 +77,26 @@ def predict():
         logger.info(f'prediction: {prediction_id}/{original_img_path}. prediction summary:\n\n{labels}')
 
         prediction_summary = {
-            'prediction_id': prediction_id,
+            'prediction_id': str(prediction_id),
             'original_img_path': original_img_path,
-            'predicted_img_path': predicted_img_path,
+            'predicted_img_path': str(predicted_img_path),
             'labels': labels,
             'time': time.time()
         }
 
-        # TODO store the prediction_summary in MongoDB
+        # Store the prediction_summary in MongoDB
+        client = pymongo.MongoClient("mongodb://mongo1:27017/")
+        db = client["mongodb"]
+        collection = db["prediction"]
 
-        return prediction_summary
+        inserted_id = collection.insert_one(prediction_summary).inserted_id
+
+        # Now convert the ObjectId to str for JSON serialization
+        prediction_summary['_id'] = str(inserted_id)
+
+        return jsonify(prediction_summary)
     else:
-        return f'prediction: {prediction_id}/{original_img_path}. prediction result not found', 404
+        return jsonify({'error': f'prediction: {prediction_id}/{original_img_path}. prediction result not found'}), 404
 
 
 if __name__ == "__main__":
