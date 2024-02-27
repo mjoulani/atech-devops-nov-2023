cf9428a5705e319e77b7ab434c2a0b1ae0797e91
ofirbakria <ofirbakria@gmail.com>
2024-02-27 12:14:21 +0200

    start building the polyboy image

--- docker_project/init.sh
+++ docker_project/init.sh
@@ -7,10 +7,12 @@
 ngrok http 8443 > ngrok_output.log 2>&1 &
 
 # Wait for a few seconds to allow ngrok to start (adjust as needed)
+echo "Please wait ...."
 sleep 5
-
-# Read the ngrok output from the log file and extract the public URL
-public_url=$(grep -oE 'Forwarding\s+https?://[^ ]+' ngrok_output.log)
+# Run curl command to retrieve JSON data from ngrok
+json_data=$(curl -s -g -X GET "http://localhost:4040/api/tunnels")
+public_url=$(echo "$json_data" | jq -r '.tunnels[0].public_url')
 
 # Print the public URL
-echo "Public URL: $public_url"
+export PUBLIC_IP=$public_url
+echo $PUBLIC_IP
