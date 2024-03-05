fade50e4b7e3b2080f59d1ea3bb99545ccfb093a
WaseemAdmin <waseemaboasaid2000@gmail.com>
2024-03-05 19:01:12 +0200

    create branch for docker project

--- aws/load_balancer/apache.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install Apache
-yam update -y
-yum install -y httpd
-echo "Hello World from $(hostname -f)" > /var/www/html/index.html
-sudo service httpd start
-sudo chkconfig httpd on
-
--- docker/mongo_jenkins/mongo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Pull the latest MongoDB image is not necessary
-docker pull mongo:latest
-
-# Run the MongoDB container
-docker run --name mongodb -p 27017:27017 -v E:\elements\Devops_Atech\mongo_data:/data/db -e MONGO_INITDB_ROOT_USERNAME=admin -e MONGO_INITDB_ROOT_PASSWORD=password -d  mongo:latest
