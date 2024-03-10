0afc8e3895e4f53eb0ac03a5028ff028f56b39d8
alexey <mihaylov>
2024-03-10 18:03:49 +0200

    add apache and loadtest

--- aws/load_balancer/apache.sh
+++ aws/load_balancer/apache.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Install Apache
-yam update -y
-yum install -y httpd
+sudo yam update -y
+sudo yum install -y httpd
 echo "Hello World from $(hostname -f)" > /var/www/html/index.html
 sudo service httpd start
 sudo chkconfig httpd on
