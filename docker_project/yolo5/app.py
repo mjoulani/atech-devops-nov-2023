4fcb2cc0f99929c63376fbe210d7b6ecabbd7e06
ofirbakria <ofirbakria@gmail.com>
2024-03-10 18:52:47 +0200

    last version

--- docker_project/yolo5/app.py
+++ docker_project/yolo5/app.py
@@ -16,9 +16,11 @@ with open("data/coco128.yaml", "r") as stream:
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def index():
     return "Hello from yolo5"
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -64,43 +66,57 @@ def predict():
     s3.upload_file(str(predicted_img_path), bucket_name, the_image)
 
     # Parse prediction labels and create a summary
-    pred_summary_path = Path(f'static/data/{prediction_id}/labels/{original_img_path.split(".")[0]}.txt')
-    if pred_summary_path.exists():
-        with open(pred_summary_path) as f:
-            labels = f.read().splitlines()
-            labels = [line.split(' ') for line in labels]
-            labels = [{
-                'class': names[int(l[0])],
-                'cx': float(l[1]),
-                'cy': float(l[2]),
-                'width': float(l[3]),
-                'height': float(l[4]),
-            } for l in labels]
 
-        logger.info(f'prediction: {prediction_id}/{original_img_path}. prediction summary:\n\n{labels}')
+    try:
+        pred_summary_path = Path(f'static/data/{prediction_id}/labels/{original_img_path.split(".")[0]}.txt')
 
-        prediction_summary = {
-            'prediction_id': str(prediction_id),
-            'original_img_path': original_img_path,
-            'predicted_img_path': str(predicted_img_path),
-            'labels': labels,
-            'time': time.time()
-        }
-        
-        #Store the prediction_summary in MongoDB
-        client = pymongo.MongoClient("mongodb://mongo1:27017/")
-        db = client["mongodb"]
-        collection = db["prediction"]
+        if pred_summary_path.exists():
+            with open(pred_summary_path) as f:
+                labels = f.read().splitlines()
+                labels = [line.split(' ') for line in labels]
+                labels = [{
+                    'class': names[int(l[0])],
+                    'cx': float(l[1]),
+                    'cy': float(l[2]),
+                    'width': float(l[3]),
+                    'height': float(l[4]),
+                } for l in labels]
 
-        inserted_id = collection.insert_one(prediction_summary).inserted_id
+            logger.info(f'prediction: {prediction_id}/{original_img_path}. prediction summary:\n\n{labels}')
 
-        # Now convert the ObjectId to str for JSON serialization
-        prediction_summary['_id'] = str(inserted_id)
-        return jsonify(prediction_summary)
-        
-    else:
-        return jsonify({'error': f'prediction: {prediction_id}/{original_img_path}. prediction result not found'}), 404
+            prediction_summary = {
+                'prediction_id': str(prediction_id),
+                'original_img_path': original_img_path,
+                'predicted_img_path': str(predicted_img_path),
+                'labels': labels,
+                'time': time.time()
+            }
 
+            # Store the prediction_summary in MongoDB
+            client = pymongo.MongoClient("mongodb://mongo1:27017/")
+            db = client["mongodb"]
+            collection = db["prediction"]
+
+            inserted_id = collection.insert_one(prediction_summary).inserted_id
+
+            # Now convert the ObjectId to str for JSON serialization
+            prediction_summary['_id'] = str(inserted_id)
+            return jsonify(prediction_summary)
+
+        else:
+            return jsonify({
+                'prediction_id': str(prediction_id),
+                'original_img_path': original_img_path,
+                'predicted_img_path': str(predicted_img_path),
+                'labels': [],
+                'time': time.time()
+            }), 404
+
+    except Exception as e:
+        # Log the exception or handle it as needed
+        logger.error(f'An error occurred while processing prediction: {prediction_id}/{original_img_path}. Error: {e}')
+        return jsonify({
+                           'error yolo5 app': f'An error occurred while processing prediction: {prediction_id}/{original_img_path}'}), 500
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8081)
