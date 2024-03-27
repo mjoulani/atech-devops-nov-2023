7d3fbc8126dc7d83a0228c983975940dfe1c7d2d
ofirbakria <ofirbakria@gmail.com>
2024-03-27 20:16:30 +0200

    after first jenkinns with S3

--- jenkins/pipelines/dowload_s3.groovy
+++ jenkins/pipelines/dowload_s3.groovy
@@ -2,12 +2,14 @@ properties([
         parameters([
                 string(name: 'AWS_CRED', defaultValue: 'aws_cred', description: 'AWS Credentials ID'),
                 string(name: 'S3_BUCKET', defaultValue: 'oferbakria', description: 'S3 Bucket Name'),
-                string(name: 'S3_FILE', defaultValue: '', description: 'S3 File Name'),
+                string(name: 'S3_FILE', defaultValue: 'file_12.jpg', description: 'S3 File Name'),
                 string(name: 'DEST', defaultValue: '${WORKSPACE}', description: 'Destination Folder'),
                 string(name: 'REGION', defaultValue: 'ap-southeast-2', description: 'AWS Region'),
-              
+            
                 // booleanParam(name: 'DEBUG', defaultValue: true, description: 'Enable Debugging'),
+
                 // choice(name: 'CHOICE', choices: ['main', 'development', 'feature'], description: 'Choose one'),
+
                 // [$class: 'CascadeChoiceParameter', choiceType: 'PT_CHECKBOX', filterLength: 1, filterable: false,
                 //              name: 'MODULES', referencedParameters: 'AWS_CRED',
                 //              script: [$class: 'GroovyScript', fallbackScript: [classpath: [], oldScript: '', sandbox: true, script: 'return [\'error\']'],
@@ -29,7 +31,7 @@ pipeline {
             steps {
                 script{
                     println("=====================================${STAGE_NAME}=====================================")
-                    download_from_s3(param.AWS_CRED,param.S3_BUCKET,param.S3_FILE,"${WORKSPACE}")  
+                    download_from_s3(params.AWS_CRED,params.S3_BUCKET,params.S3_FILE,"${WORKSPACE}")  
                 }
             }
         }
@@ -39,7 +41,7 @@ pipeline {
 
 //============================================================FUNC=====================================================
 
-def download_from_s3(def cred,def bucket, def file, def dest,def region=param.REGION){
+def download_from_s3(def cred,def bucket, def file, def dest,def region=params.REGION){
     withAWS(credentials: cred, region: region ) {
         println("Downloading file name ${file} from bucket ${bucket} to ${dest}")
         // sh "aws s3 cp s3://${bucket}/${file} ${dest}" 
