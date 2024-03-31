a0ec58e1d491fce916a17282f3c7de277fe62c16
alexey <mihaylov>
2024-03-31 17:35:21 +0300

    up

--- jenkins/pipelines/generic_webhook.groovy
+++ jenkins/pipelines/generic_webhook.groovy
@@ -1,8 +1,16 @@
 //http://localhost:8080/jenkins/generic-webhook-trigger/invoke?token=abc123
 
+
+PROP = [:]
+
+PROP['git_cred'] = 'github_ssh_key'
+PROP['branch'] = 'main'
+
+
+
 pipeline {
     agent any
-    triggers {
+        triggers {
         GenericTrigger(
                 genericVariables: [
                         [key: 'refsb', value: '$.changes[0].ref.id'],
@@ -21,6 +29,15 @@ pipeline {
         )
     }
     stages {
+        stage('Git clone') {
+            steps {
+                script{
+                    println("=====================================${STAGE_NAME}=====================================")
+                    println("Cloning from branch ${PROP.branch} and using credentials ${PROP.git_cred}")
+                    git branch: PROP.branch, credentialsId: PROP.git_cred, url: 'git@github.com:AlexeyMihaylovDev/atech-devops-nov-2023.git'
+                }
+            }
+        }
         stage('Build') {
             steps {
                 echo 'Building..'
