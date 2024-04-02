ac49d84986759fef87ee958f20da313040363660
AlexeyMihaylovDev <99612157+AlexeyMihaylovDev@users.noreply.github.com>
2024-04-02 18:46:10 +0300

    Update script.sh

--- jenkins/install/script.sh
+++ jenkins/install/script.sh
@@ -18,6 +18,7 @@ docker run -p 8080:8080 -p 50000:50000 -v /var/jenkins_home:/var/jenkins_home -d
 
 echo 'pass for start jenkins: ' $(/var/jenkins_home/secrets/initialAdminPassword)
 
+
 # show endpoint
 echo 'Jenkins installed'
 echo 'You should now be able to access jenkins at: http://'$(curl -s ifconfig.co)':8080'
