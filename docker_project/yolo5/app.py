8b647e72d1e7c8e6b106c8194b5074c75b04c781
AhmadBaloum <ahmad.baloum4@gmail.com>
2024-04-05 04:17:04 +0300

    first commit (uncomplete)

--- docker_project/yolo5/app.py
+++ docker_project/yolo5/app.py
@@ -1,11 +1,13 @@
 import time
 from pathlib import Path
-from flask import Flask, request
+import boto3
+from flask import Flask, request, jsonify
 from detect import run
 import uuid
 import yaml
 from loguru import logger
 import os
+from pymongo import MongoClient
 
 images_bucket = os.environ['BUCKET_NAME']
 
@@ -26,7 +28,13 @@ def predict():
 
     # TODO download img_name from S3, store the local image path in original_img_path
     #  The bucket name should be provided as an env var BUCKET_NAME.
-    original_img_path = ...
+
+    bucket_name = os.getenv('BUCKET_NAME')
+
+    original_img_path = str(img_name)
+
+    s3 = boto3.client('s3')
+    s3.download_file(bucket_name, img_name, original_img_path)
 
     logger.info(f'prediction: {prediction_id}/{original_img_path}. Download img completed')
 
@@ -48,6 +56,9 @@ def predict():
 
     # TODO Uploads the predicted image (predicted_img_path) to S3 (be careful not to override the original image).
 
+    the_image = original_img_path[:-4] + "predicted.jpg"
+    s3.upload_file(str(predicted_img_path), bucket_name, the_image)
+
     # Parse prediction labels and create a summary
     pred_summary_path = Path(f'static/data/{prediction_id}/labels/{original_img_path.split(".")[0]}.txt')
     if pred_summary_path.exists():
@@ -65,14 +76,20 @@ def predict():
         logger.info(f'prediction: {prediction_id}/{original_img_path}. prediction summary:\n\n{labels}')
 
         prediction_summary = {
-            'prediction_id': prediction_id,
+            'prediction_id': str(prediction_id),
             'original_img_path': original_img_path,
-            'predicted_img_path': predicted_img_path,
+            'predicted_img_path': str(predicted_img_path),
             'labels': labels,
             'time': time.time()
         }
 
         # TODO store the prediction_summary in MongoDB
+
+        client = MongoClient("mongodb://127.0.0.1:27017/")
+        db = client["projectdb"]
+        collection = db["your_collection_name"]
+
+        collection.insert_one(my_dict)
 
         return prediction_summary
     else:
