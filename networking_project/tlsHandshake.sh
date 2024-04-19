8e964a368765d121ac67f6c9d6d5fff21ea93b51
daniel <dmakhalfeh@gmail.com>
2024-04-19 22:12:15 +0300

    Final Project

--- networking_project/tlsHandshake.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-# TODO Your solution here
--- theatre_nighout/init.sh
+++
@@ -1,6 +0,0 @@
-rm -f -r shows
-for i in 'The Lion King' 'Hamilton' 'Wicked' 'Les Miserables' 'Phantom of the Opera'
-do
-  mkdir -p shows/"$i"
-  for j in $(seq 1 50)
-  do
