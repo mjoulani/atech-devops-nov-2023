808023bedad4b52e583bfdc2d7e41d88e89eecf0
daniel <dmakhalfeh@gmail.com>
2024-04-21 00:40:54 +0300

    Working 1st step

--- aws_project/polybot/app.py
+++ aws_project/polybot/app.py
@@ -1,18 +1,20 @@
 import flask
 from flask import request
 import os
-from bot import ObjectDetectionBot, Bot, QuoteBot
+from bot import ObjectDetectionBot
 
 app = flask.Flask(__name__)
 
-TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
-TELEGRAM_APP_URL = os.environ['TELEGRAM_APP_URL']
+
+# TODO load TELEGRAM_TOKEN value from Secret Manager
+TELEGRAM_TOKEN = '6521616754:AAGPxWVhiBfOKZSwWJ4THvVreggYD5S9Keg'
+
+TELEGRAM_APP_URL = 'https://Daniel-LB-1354148717.eu-west-1.elb.amazonaws.com'
 
 
 @app.route('/', methods=['GET'])
 def index():
-    return 'Ok'
-
+    return 'Ok normal'
 
 
 @app.route(f'/{TELEGRAM_TOKEN}/', methods=['POST'])
@@ -22,8 +24,27 @@ def webhook():
     return 'Ok'
 
 
+@app.route(f'/results/', methods=['GET'])
+def results():
+    prediction_id = request.args.get('predictionId')
+
+    # TODO use the prediction_id to retrieve results from DynamoDB and send to the end-user
+
+    chat_id = ...
+    text_results = ...
+
+    bot.send_text(chat_id, text_results)
+    return 'Ok'
+
+
+@app.route(f'/loadTest/', methods=['POST'])
+def load_test():
+    req = request.get_json()
+    bot.handle_message(req['message'])
+    return 'Ok'
+
+
 if __name__ == "__main__":
-    #bot = Bot(TELEGRAM_TOKEN, TELEGRAM_APP_URL)
     bot = ObjectDetectionBot(TELEGRAM_TOKEN, TELEGRAM_APP_URL)
 
     app.run(host='0.0.0.0', port=8443)
--- aws_project/polybot/bot.py
+++ aws_project/polybot/bot.py
@@ -1,14 +1,8 @@
-import uuid
-import requests
 import telebot
-from botocore.exceptions import NoCredentialsError
 from loguru import logger
 import os
 import time
 from telebot.types import InputFile
-import boto3
-
-images_bucket = "daniel-yolo"
 
 
 class Bot:
@@ -23,8 +17,8 @@ class Bot:
         time.sleep(0.5)
 
         # set the webhook URL
-        self.telegram_bot_client.set_webhook(url=f'{telegram_chat_url}/{token}/', timeout=60)
-
+        self.telegram_bot_client.set_webhook(url=f'{telegram_chat_url}/{token}/', certificate=open("YOURPUBLIC.pem", 'r'), timeout=60)
+#certificate=open("YOURPUBLIC.pem", 'r'),
         logger.info(f'Telegram Bot information\n\n{self.telegram_bot_client.get_me()}')
 
     def send_text(self, chat_id, text):
@@ -33,8 +27,7 @@ class Bot:
     def send_text_with_quote(self, chat_id, text, quoted_msg_id):
         self.telegram_bot_client.send_message(chat_id, text, reply_to_message_id=quoted_msg_id)
 
-    @staticmethod
-    def is_current_msg_photo(msg):
+    def is_current_msg_photo(self, msg):
         return 'photo' in msg
 
     def download_user_photo(self, msg):
@@ -72,94 +65,13 @@ class Bot:
         self.send_text(msg['chat']['id'], f'Your original message: {msg["text"]}')
 
 
-class QuoteBot(Bot):
-    def handle_message(self, msg):
-        logger.info(f'Incoming message: {msg}')
-
-        if msg["text"] != 'Please don\'t quote me':
-            self.send_text_with_quote(msg['chat']['id'], msg["text"], quoted_msg_id=msg["message_id"])
-
-
 class ObjectDetectionBot(Bot):
     def handle_message(self, msg):
         logger.info(f'Incoming message: {msg}')
 
         if self.is_current_msg_photo(msg):
-            # TODO download the user photo (utilize download_user_photo)
             photo_path = self.download_user_photo(msg)
+
             # TODO upload the photo to S3
-            logger.info(f'Photo uploaded to S3. S3 URL started : s3://{images_bucket}/{photo_path}')
-            image_id = self.upload_to_s3(photo_path, images_bucket)
-            # TODO send a request to the `yolo5` service for prediction
-            logger.info(f'Photo is sending to yolov')
-            prediction_result = self.predict_with_yolov(image_id)
-            logger.info(f'sleep mode on ')
-            time.sleep(10)
-            logger.info(f'sleep mode off ')
-
-            # TODO send results to the Telegram end-user
-            self.send_text(msg['chat']['id'], f'Prediction Result: {prediction_result}')
-            logger.info(f'image_id_outside {image_id}')
-            image_id_new = image_id[:-4] + "_predicted.jpg"
-            #download image and send image path = photos/
-            predict_img_path = photo_path.split('/')[0]
-            final_image_predict_path = predict_img_path +"/"+ image_id_new
-
-            s3 = boto3.client('s3')
-            try:
-                s3.download_file(images_bucket, image_id_new, final_image_predict_path)
-                self.send_photo(msg['chat']['id'],final_image_predict_path)
-            except Exception as e:
-                logger.error(f"Error downloading predicted photo to S3: {e}")
-
-    def upload_to_s3(self, local_path, images_bucket):
-        s3 = boto3.client('s3')
-
-        image_id = str(uuid.uuid4())
-        image_id = f'{image_id}.jpeg'
-        try:
-            s3.upload_file(local_path, images_bucket, image_id)
-            logger.info(f'Photo uploaded to S3. S3 URL: s3://{images_bucket}/{image_id}')
-
-            return image_id
-        except NoCredentialsError:
-            logger.error("AWS credentials not available.")
-            return None
-        except Exception as e:
-            logger.error(f"Error uploading photo to S3: {e}")
-            return None
-
-    def predict_with_yolov(self, image_id):
-        # Initialize prediction_result with a default value
-        prediction_result = None
-        logger.info(f'Sending Photo to yolov inside method. S3 URL: s3://{images_bucket}/{image_id}')
-        # Define the base URL of your Flask applicationubu
-        base_url = "http://yolov:8081"
-        #base_url = "http://10.0.0.5:8081"
-
-        # Define the endpoint
-        endpoint = "/predict"
-
-        # Combine the base URL and endpoint
-        url = base_url + endpoint
-
-        # Set the image name as a URL parameter
-        params = {'imgName': image_id}
-
-        try:
-            # Send a POST request to the /predict endpoint
-            response = requests.post(url, params=params)
-
-            # Check if the request was successful (status code 200)
-            if response.status_code == 200:
-                # Print the response JSON
-                prediction_result = response.json()
-                logger.info(f'Response 200: ****{prediction_result}******/')
-
-            else:
-                # Print an error message if the request was not successful
-                print(f"Error: {response.status_code} - {response.text}")
-
-        except requests.exceptions.RequestException as e:
-            print(f"Error sending request: {e}")
-        return prediction_result
+            # TODO send a job to the SQS queue
+            # TODO send message to the Telegram end-user (e.g. Your image is being processed. Please wait...)
