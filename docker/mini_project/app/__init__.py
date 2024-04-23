e560b37e5d2b5dfc61b0b2e2468050c7d58f3674
daniel <dmakhalfeh@gmail.com>
2024-04-24 00:35:20 +0300

    clean up

--- docker/mini_project/app/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask_pymongo import PyMongo
-
-app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb://admin:password@mongodb:27017/mydatabase'
-mongo = PyMongo(app)
-
