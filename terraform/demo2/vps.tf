c3e28005711dd7490ddec95cbf1157623a691c78
Ubuntu <ubuntu@ip-10-10-1-25.eu-central-1.compute.internal>
2024-04-30 08:58:20 +0000

    abedAWS234

--- terraform/demo2/vps.tf
+++
@@ -1,9 +0,0 @@
-# tflint:disable terraform_required_providers
-resource "aws_vpc" "vpc" {
-  cidr_block       = var.cidr_block_vpc
-  instance_tenancy = "default"
-
-  tags = {
-    Name = format("%s-tf", var.tagging)
-  }
-}
