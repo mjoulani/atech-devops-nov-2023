fc410cfd50ca51384ab6d2b54c3511d54dc0b562
AhmadBaloum <ahmad.baloum4@gmail.com>
2024-05-02 01:34:12 +0300

    first (uncomplete)

--- aws_project/polybot/app.py
+++ aws_project/polybot/app.py
@@ -2,12 +2,38 @@ import flask
 from flask import request
 import os
 from bot import ObjectDetectionBot
+import boto3
+from botocore.exceptions import ClientError
+from loguru import logger
 
 app = flask.Flask(__name__)
 
 
 # TODO load TELEGRAM_TOKEN value from Secret Manager
-TELEGRAM_TOKEN = ...
+
+def get_secret():
+    secret_name = "telegram-polybot-token"
+    region_name = "ap-northeast-2"
+
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        raise e
+
+    secret = get_secret_value_response['SecretString']
+    secret = json.loads(secret)
+
+    return secret['TELEGRAM_TOKEN']
+
+TELEGRAM_TOKEN = get_secret()
 
 TELEGRAM_APP_URL = os.environ['TELEGRAM_APP_URL']
 
