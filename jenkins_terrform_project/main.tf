568e51604c937bc3b2c8889a18a279f87d8d26fc
mjoulani <muhamed_joulani@hotmail.com>
2024-05-12 07:53:06 +0300

    i run the code without saving running agin

--- jenkins_terrform_project/main.tf
+++ jenkins_terrform_project/main.tf
@@ -15,18 +15,6 @@ output "tf_var_zone_output" {
 }
 
 
-provider "null" {}
-
-resource "null_resource" "output_zone" {
-  provisioner "local-exec" {
-    command = "echo ${var.zone}"
-  }
-}
-
-output "tf_var_zone_output" {
-  value = var.zone
-}
-
 
 
 
