95373772067e2aa782ef1e5506b85076d4354855
mjoulani <muhamed_joulani@hotmail.com>
2024-05-12 07:50:11 +0300

    add the miss code for the plan

--- jenkins_terrform_project/main.tf
+++ jenkins_terrform_project/main.tf
@@ -1,3 +1,20 @@
+variable "zone" {
+  description = "Zone choice"
+}
+
+provider "null" {}
+
+resource "null_resource" "output_zone" {
+  provisioner "local-exec" {
+    command = "echo ${var.zone}"
+  }
+}
+
+output "tf_var_zone_output" {
+  value = var.zone
+}
+
+
 provider "null" {}
 
 resource "null_resource" "output_zone" {
