e9397d4477ea38f75fe8e9c11c830a41aa991510
mjoulani <muhamed_joulani@hotmail.com>
2024-05-12 07:25:46 +0300

    try to run the test plan

--- jenkins_terrform_project/main.tf
+++ jenkins_terrform_project/main.tf
@@ -1,13 +1,19 @@
 provider "null" {}
 
+variable "zone" {
+  default = var.TF_VAR_zone
+}
+
 resource "null_resource" "output_zone" {
   provisioner "local-exec" {
-    command = "echo ${var.TF_VAR_zone}"
+    command = "echo ${var.zone}"
   }
 }
 
 output "tf_var_zone_output" {
-  value = var.TF_VAR_zone
+  value = var.zone
 }
+
+
 
 
