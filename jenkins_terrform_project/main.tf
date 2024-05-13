0007a66e3fb2125c7ecc068393e24b1af24e3389
mjoulani <muhamed_joulani@hotmail.com>
2024-05-13 06:06:05 +0300

    starting terraform

--- jenkins_terrform_project/main.tf
+++ jenkins_terrform_project/main.tf
@@ -1,3 +1,25 @@
+# tf/main.tf
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=5.0"
+    }
+  }
+
+    required_version = ">= 1.7.0"
+}
+
+provider "aws" {
+  region  = var.zone
+}
+  
+
+
+
+
+
 variable "zone" {
   description = "Zone choice"
 }
