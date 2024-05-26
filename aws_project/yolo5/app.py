ce4c477c3238198fbdac5117327b93fea4299359
daniel <dmakhalfeh@gmail.com>
2024-05-26 18:00:16 +0300

    Done with auto cloudwatch

--- aws_project/yolo5/app.py
+++ aws_project/yolo5/app.py
@@ -44,6 +44,48 @@ def consume():
             chat_id = message['chat_id']  # TODO extract from `message` DONE
             logger.info(f'\n\n\nMessage got from sqs :image name  {img_name}. chat_id: {chat_id}')
 
+            #check how many messages in queue
+            sqs_client = boto3.resource('sqs', region_name='eu-west-1')
+            asg_client = boto3.client('autoscaling', region_name='eu-west-1')
+
+            AUTOSCALING_GROUP_NAME = 'Daniel-as'
+            QUEUE_NAME = 'Daniel-sqs'
+
+            queue = sqs_client.get_queue_by_name(QueueName=QUEUE_NAME)
+            msgs_in_queue = int(queue.attributes.get('ApproximateNumberOfMessages'))
+            asg_groups = asg_client.describe_auto_scaling_groups(AutoScalingGroupNames=[AUTOSCALING_GROUP_NAME])[
+                'AutoScalingGroups']
+
+            if not asg_groups:
+                raise RuntimeError('Autoscaling group not found')
+            else:
+                asg_size = asg_groups[0]['DesiredCapacity']
+
+            backlog_per_instance = msgs_in_queue / asg_size
+
+            # TODO send backlog_per_instance to cloudwatch...
+
+            # Create CloudWatch client
+            cloudwatch = boto3.client('cloudwatch', region_name='eu-west-1')
+
+            # Put custom metrics
+            cloudwatch.put_metric_data(
+                MetricData=[
+                    {
+                        'MetricName': 'danielmetric',
+                        'Dimensions': [
+                            {
+                                'Name': 'queue',
+                                'Value': 'backlog_per_instance'
+                            },
+                        ],
+                        'Unit': 'Count',
+                        'Value': backlog_per_instance
+                    },
+                ],
+                Namespace='daniel'
+            )
+
             # TODO download img_name from S3, store the local image path in original_img_path
             s3 = boto3.client('s3')
             original_img_path = str(img_name)
