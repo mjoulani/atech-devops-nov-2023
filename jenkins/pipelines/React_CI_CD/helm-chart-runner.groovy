8244f4290d97a59d0a2d44a4b33fae91bf3b683e
alexey <alexeymihaylovdev@gmail.com>
2024-06-06 19:03:44 +0300

    update pipeline

--- jenkins/pipelines/React_CI_CD/helm-chart-runner.groovy
+++ jenkins/pipelines/React_CI_CD/helm-chart-runner.groovy
@@ -5,12 +5,28 @@ import groovy.transform.Field
 
 @Field JOB = [:]
 
+JOB.trigg_by = currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause').userName
+
 pipeline {
 
-    agent { label params.AGENT}
-
-
-
+    agent any
+    triggers {
+        GenericTrigger(
+                genericVariables: [
+                        [key: 'refsb', value: '$.ref'],
+                        [key: 'pusher', value: '$.pusher.name'],
+                        [key: 'change_files', value: '$.commits[0].modified[0]'],
+                        // [key: 'type', value: '$.changes[0].type'],
+                ],
+                token: "123456",
+                tokenCredentialId: '',
+                printContributedVariables: true,
+                printPostContent: false,
+                silentResponse: false,
+                regexpFilterText: '$refsb $change_files',
+                regexpFilterExpression: '^(refs/heads/main)'        
+        )
+    }
     options {
         buildDiscarder(logRotator(numToKeepStr: '5'))
         timeout(time: 1, unit: 'HOURS')
@@ -20,60 +36,90 @@ pipeline {
     stages{
         stage('Git clone'){
             steps{
-                git branch: 'main', url: 'https://github.com/AlexeyMihaylovDev/atech-devops-nov-2023.git'
+                checkout([$class: 'GitSCM', branches: [[name: '*/main']], userRemoteConfigs: [[url: 'https://github.com/AlexeyMihaylovDev/atech-devops-nov-2023']]])    
             }
         }
         stage('Read helm chart values'){
             steps{
-                script{
-
-                    dir('k8s\helm\react_site'){
-                    def chartValues = readYaml file: 'values.yaml'    
+                script{  
+                    
+                     dir("${WORKSPACE}/k8s/helm/react_site")  {
+                     sh 'ls -l'    
+                    def chartValues = readFile (file: 'values.yaml')  
                     JOB.buildChartValues = chartValues
-                    }               
+                     }
+                    println(JOB.buildChartValues)
+                                
                 }
                 
             }
          }
-        }
         stage('Input params '){
+            when{
+                expression { !JOB.trigg_by.isEmpty()}
+            }
             steps{
                 script{
                     def userInput = input (id: 'userInput', message: 'Please provide the following parameters', parameters: [
                         [$class: 'WHideParameterDefinition', defaultValue: JOB.buildChartValues, description: '', name: 'chartValues'],
+                        choice(name: 'CHOICE_HELM', choices: ['install','upgrade','uninstall'], description: 'Choose one'),
                         [$class: 'DynamicReferenceParameter', choiceType: 'ET_FORMATTED_HTML', description: "",
                              name: 'ChartValues', omitValueField: true, referencedParameters: 'chartValues',
                              script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: true,
                                                                                script: 'return [\'error\']'], script: [classpath: [], sandbox: true, script: '''
                                 def jsonValue = "${chartValues}".replaceAll('"', '\\\\"') 
                                 return "<textarea name=\\"value\\" rows='27' cols='100'>${jsonValue}</textarea>"
-                            ''']]],                            
+                            ''']]],
+
                     ])
-                    JOB.params = readYaml text: userInput['ChartValues']
+                    JOB.params = userInput['ChartValues']
+                    sh 'rm -f values.yaml' //delete old values.yaml
+
                 }
             }
         }
         stage('Helm update values by user input'){
+            when{
+                expression { !JOB.trigg_by.isEmpty()}
+            }
             steps{
                 script{
-                    dir('k8s\helm\react_site'){
-                        sh 'rm -f values.yaml' //delete old values.yaml
-                        def chartValues = JOB.buildChartValues
-                        def params = JOB.params
-                        def updatedChartValues = chartValues + params
-                        writeFile file: 'values.yaml', text: JOB.params.toYaml()
+                     dir("${WORKSPACE}/k8s/helm/react_site")  {
+                        writeFile file: 'values.yaml', text: JOB.params
                     }
                 }
             }
         }
+        stage('Run is Job with Auto Trigger'){
+            when{
+                expression { JOB.trigg_by.isEmpty()
+                }
+            }
+            steps{
+                script{
+                    println("=====================================${STAGE_NAME}=====================================")
+                }
+            }
+        }
+        
+        // stage('Login to EKS'){
+        //     steps{
+        //         script{
+        //             withCredentials([string(credentialsId: 'aws-eks-creds', variable: 'AWS_CREDENTIALS')]) {
+        //                 sh 'aws eks --region us-west-2 update-kubeconfig --name alexey'
+        //             }
+        //         }
+        //     }
+        // }
+
         stage('Helm install'){
             steps{
                 script{
-                    dir('k8s\helm\react_site'){
-                        sh 'helm install react-site . -f values.yaml'
+                     dir("${WORKSPACE}/k8s/helm/react_site")  {
+                        sh 'helm install react-site . -f values.yaml -n alexey'
                     }
                 }
             }
         }
-
+        }
     }
