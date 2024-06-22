36fb63468cfce3162df6adb4a5a365afd15fc90f
ofirbakria <ofirbakria@gmail.com>
2024-06-22 14:11:13 +0300

    starting groovy file

--- final/piplines/pipline.groovy
+++ final/piplines/pipline.groovy
@@ -1,43 +1,231 @@
+import groovy.json.JsonBuilder
+import groovy.json.JsonSlurper
+import groovy.json.JsonSlurperClassic
+import groovy.transform.Field
+
+@Field JOB = [:]
+
+JOB.trigg_by = currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause').userName
+
 PROP = [:]
 
 PROP['git_cred'] = 'github'
 PROP['branch'] = 'oferbakria'
-PROP['docker_tag'] = 'latest'
-PROP['proj_url'] = 'https://github.com/ofirbakria/jenkins_project'
+PROP['proj_url'] = 'https://github.com/AlexeyMihaylovDev/atech-devops-nov-2023'
+
+PROP['dockerhub_cred'] = 'dockerhub_cred' // Jenkins credential ID for DockerHub
+PROP['docker_image1'] = 'oferbakria/poly'
+PROP['docker_image2'] = 'oferbakria/yolo'
+PROP['docker_tag'] = '1.1'
+PROP['aws_cli_cred'] = 'aws_cred'
 
 // PROP['docker_file_path'] = 'flask/Dockerfile'
 // PROP['ecr_registry'] = '933060838752.dkr.ecr.eu-west-1.amazonaws.com/ofer'
 // PROP['aws_region'] = 'eu-west-1'
-// PROP['aws_cli_cred'] = 'aws_cred'
 // PROP['image_name'] = 'ofer-flask-image'
 
 // PROP['email_recepients'] = "alexeymihaylovdev@gmail.com"
 
 pipeline {
+
     agent { label 'jenkins_ec2' } // Use the label of your EC2 agent
+
+    // triggers {
+    //     GenericTrigger(
+    //             genericVariables: [
+    //                     [key: 'refsb', value: '$.ref'],
+    //                     [key: 'pusher', value: '$.pusher.name'],
+    //                     [key: 'change_files', value: '$.commits[0].modified[0]'],
+    //                     // [key: 'type', value: '$.changes[0].type'],
+    //             ],
+    //             token: "123456",
+    //             tokenCredentialId: '',
+    //             printContributedVariables: true,
+    //             printPostContent: false,
+    //             silentResponse: false,
+    //             regexpFilterText: '$refsb $change_files',
+    //             regexpFilterExpression: '^(refs/heads/main)'        
+    //     )
+    // }
+
+
     stages {
-        stage('Git clone') {
-            steps {
-                script{
-                    println("=====================================${STAGE_NAME}=====================================")
-                    println("Cloning from branch ${PROP.branch} and using credentials ${PROP.git_cred}")
-                    git branch: PROP.branch, credentialsId: PROP.git_cred, url: PROP.proj_url
-                }
+
+    // stage('Hello'){
+    //     steps {
+    //         script {
+    //             println("=====================================${STAGE_NAME}=====================================")
+    //             println("Hello ${PROP.trigg_by}")
+    //         }
+    //     }
+    // }
+
+
+
+
+
+
+
+
+    stage('Git clone') {
+        steps {
+            script {
+                println("=====================================${STAGE_NAME}=====================================")
+                println("Cloning from branch ${PROP.branch} and using credentials ${PROP.git_cred}")
+
+                // Enable sparse checkout
+                checkout([
+                    $class: 'GitSCM', 
+                    branches: [[name: "*/${PROP.branch}"]],
+                    doGenerateSubmoduleConfigurations: false, 
+                    extensions: [
+                        [$class: 'SparseCheckoutPaths', sparseCheckoutPaths: [[path: 'final']]]
+                    ], 
+                    submoduleCfg: [], 
+                    userRemoteConfigs: [[
+                        credentialsId: PROP.git_cred, 
+                        url: PROP.proj_url
+                    ]]
+                ])
             }
+        }
         }
 
 
-        // stage('Create Docker Image') {
+
+    // stage('Create Docker Image') {
+    //     steps {
+    //          script {
+    //          println("=====================================${STAGE_NAME}=====================================")
+    //             sh "docker build -t ${PROP.docker_image1}:${PROP.docker_tag} ./final/aws_project/polybot"
+    //             sh "docker build -t ${PROP.docker_image2}:${PROP.docker_tag} ./final/aws_project/yolo5"
+    //          }
+    //     }
+    // }
+
+    // stage('Push Docker Images') {
+    //     steps {
+    //         script {
+    //             println("=====================================${STAGE_NAME}=====================================")
+    //             withCredentials([usernamePassword(credentialsId: PROP.dockerhub_cred, passwordVariable: 'DOCKERHUB_PASS', usernameVariable: 'DOCKERHUB_USER')]) {
+
+    //                 sh """
+    //                     echo $DOCKERHUB_PASS | docker login -u $DOCKERHUB_USER --password-stdin
+    //                     docker push ${PROP.docker_image1}:${PROP.docker_tag}
+    //                     docker push ${PROP.docker_image2}:${PROP.docker_tag}
+
+    //                     docker rmi -f ${PROP.docker_image1}:${PROP.docker_tag}
+    //                     docker rmi -f ${PROP.docker_image2}:${PROP.docker_tag}
+    //                 """
+    //             }
+    //         }
+    //     }
+    // }
+
+
+
+    // stage('Read helm chart values'){
+    //     steps{
+    //         script{  
+    //                 dir("${WORKSPACE}/final")  {
+    //                 sh 'ls -l'    
+    //             def chartValues = readFile (file: 'values.yaml')  
+    //             JOB.buildChartValues = chartValues
+    //                 }
+    //             println(JOB.buildChartValues)
+                            
+    //         }
+            
+    //     }
+    // }
+
+
+
+    // stage('Input params '){
+    //     when{
+    //         expression { !JOB.trigg_by.isEmpty()}
+    //     }
+    //     steps{
+    //         script{
+    //             def userInput = input (id: 'userInput', message: 'Please provide the following parameters', parameters: [
+    //                 [$class: 'WHideParameterDefinition', defaultValue: JOB.buildChartValues, description: '', name: 'chartValues'],
+    //                 choice(name: 'CHOICE_HELM', choices: ['install','upgrade','uninstall'], description: 'Choose one'),
+    //                 [$class: 'DynamicReferenceParameter', choiceType: 'ET_FORMATTED_HTML', description: "",
+    //                         name: 'ChartValues', omitValueField: true, referencedParameters: 'chartValues',
+    //                         script: [$class: 'GroovyScript', fallbackScript: [classpath: [], sandbox: true,
+    //                                                                         script: 'return [\'error\']'], script: [classpath: [], sandbox: true, script: '''
+    //                         def jsonValue = "${chartValues}".replaceAll('"', '\\\\"') 
+    //                         return "<textarea name=\\"value\\" rows='27' cols='100'>${jsonValue}</textarea>"
+    //                     ''']]],
+
+    //             ])
+    //             JOB.params = userInput['ChartValues']
+    //             sh 'rm -f values.yaml' //delete old values.yaml
+
+    //         }
+    //     }
+    // }
+
+
+
+    //     stage('Helm update values by user input'){
+    //         when{
+    //             expression { !JOB.trigg_by.isEmpty()}
+    //         }
+    //         steps{
+    //             script{
+    //                  dir("${WORKSPACE}/final")  {
+    //                     writeFile file: 'values.yaml', text: JOB.params
+    //                 }
+    //             }
+    //         }
+    //     }
+
+
+        // stage('Login to EKS') {
         //     steps {
-        //          script {
-        //          println("=====================================${STAGE_NAME}=====================================")
-        //           sh "docker build -t polybot:${PROP.docker_tag} ./my_proj/aws_project/polybot"
-        //           sh "docker build -t metricstreamer:${PROP.docker_tag} ./my_proj/aws_project/metricStreamer"
-        //          }
+        //         withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: PROP['aws_cli_cred']]]) {
+        //             script {
+        //                 sh 'aws eks --region us-east-1 update-kubeconfig --name k8s-main'
+        //             }
+        //         }
         //     }
         // }
-        
-   
-  }
+
+
+        // stage('Helm install') {
+        //     steps {
+        //         script {
+        //             def helmInstalled = sh(returnStatus: true, script: 'helm version --short')
+        //             if (helmInstalled == 0) {
+        //                 echo 'Helm is already installed.'
+        //             } else {
+        //                 echo 'Helm is not installed. Installing Helm...'
+        //                 sh """
+        //                     curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+        //                     chmod 700 get_helm.sh
+        //                     ./get_helm.sh
+        //                 """
+        //             }
+
+        //             dir("${WORKSPACE}/final") {
+        //                 sh 'helm install ofertest -f values.yaml -n oferbakria .'
+        //             }
+        //         }
+        //     }
+        // }
+
+
+
+
+
+
+
+
+
+
+    }
+
+    
 }
-// create hosts.ini file and add the public_url into the hosts file
+// install helm and kubectl add correct aws cred and add the contect of the cluster
