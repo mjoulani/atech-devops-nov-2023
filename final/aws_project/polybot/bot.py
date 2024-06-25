9f820b91461899d6fca60d2b9a1a67ed8fd7a2ab
ofirbakria <ofirbakria@gmail.com>
2024-06-25 21:20:28 +0300

    updating

--- final/aws_project/polybot/bot.py
+++ final/aws_project/polybot/bot.py
@@ -35,34 +35,6 @@ class Bot:
         logger.info(f'Telegram Bot information\n\n{self.telegram_bot_client.get_me()}')
         
 
-
-        # session = boto3.session.Session()
-        # client = session.client(
-        #     service_name='secretsmanager',
-        #     region_name="eu-west-1"
-        # )
-        # secret_response  = client.get_secret_value(
-        #     SecretId="oferbakria_certificate"
-        # )
-        # pem_contents = secret_response['SecretString']
-        # print("PEM Contents:", pem_contents)
-        # # Parse the JSON
-        # data = json.loads(pem_contents)
-
-        # # Extract the certificate value
-        # cert_value = data['poly_cert']
-        # # print(cert_value)
-        # # Write the certificate to a file
-        # file_path = "./file.pem"  # Replace with your desired file path
-        
-        # # Open the file in write mode ('w')
-        # with open(file_path, 'w') as file:
-        #     # Write the string content to the file
-        #     file.write(str(cert_value))
-
-        # # self.telegram_bot_client.set_webhook(url=f'{telegram_chat_url}/{token}/', certificate=open(file_path, 'r'))
-        # logger.info(f'Telegram Bot information\n\n{self.telegram_bot_client.get_me()}')
-
  
 
     def send_text(self, chat_id, text):
